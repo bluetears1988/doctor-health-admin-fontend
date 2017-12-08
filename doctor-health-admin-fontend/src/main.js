2f19ac92086f5909d5d5d0bf33b1d94d99ca1eda
bluetears1988 <jianghuifei123@sina.cn>
2017-12-08 20:47:40 +0800

    update

--- doctor-health-admin-fontend/src/main.js
+++ doctor-health-admin-fontend/src/main.js
@@ -1,15 +1,68 @@
+// // The Vue build version to load with the `import` command
+// // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// import Vue from 'vue'
+// import App from './App'
+// import router from './router'
+
+// Vue.config.productionTip = false
+
+// /* eslint-disable no-new */
+// new Vue({
+//   el: '#app',
+//   router,
+//   template: '<App/>',
+//   components: { App }
+// })
+
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import store from './store/'
 
-Vue.config.productionTip = false
+import axios from 'axios'
+import qs from 'qs'
+import 'font-awesome/css/font-awesome.min.css'
+
+// // axios 配置
+axios.defaults.timeout = 5000;
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8';
+// axios.defaults.baseURL = 'http://localhost:4000/';
+
+//POST传参序列化
+axios.interceptors.request.use((config) => {
+    if(config.method  === 'post'){
+        config.data = qs.stringify(config.data);
+    }
+    return config;
+},(error) =>{
+     // _.toast("错误的传参", 'fail');
+    return Promise.reject(error);
+});
+
+//返回状态判断
+axios.interceptors.response.use((res) =>{
+    if(res.data.meta.code){
+        // _.toast(res.data.msg);
+        return Promise.reject(res);
+    }
+    return res;
+}, (error) => {
+    // _.toast("网络异常", 'fail');
+    return Promise.reject(error);
+});
+
+Vue.prototype.$ajax = axios;
+Vue.prototype.$qs = qs;
+
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   template: '<App/>',
   components: { App }
 })
--- doctor-health-admin-fontend/src/router/index.js
+++ doctor-health-admin-fontend/src/router/index.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 
+import add from '../page/reports/manual/add.vue'
+import pack1 from '../page/reports/manual/pack1.vue'
+import pack2 from '../page/reports/manual/pack2.vue'
+import reportlist from '../page/reports/reportlist.vue'
+
+import orders from '../page/clients/orders.vue'
+
 Vue.use(Router)
 
 export default new Router({
@@ -10,6 +17,17 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
-    }
+    },
+    {
+      path: '/reports/manual/add',
+      component:add,
+    	children:[
+        {path: '/pack1',component:pack1,},
+        {path: '/pack2',component:pack2},
+        {path: '/page/:pack_id',component:pack2}
+    	]
+    },
+    {path: '/reports/reportlist',component:reportlist,name:'reportlist'},
+    {path: '/clients/orders',component:orders,name:'orders'},
   ]
 })
